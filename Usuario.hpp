62f431561e230b5700d42e5fe8f5da8be35a6803
ArtiePixel <luizarthursjp@gmail.com>
2025-08-14 08:59:04 -0300

    upd8 in USER class

--- Usuario.hpp
+++ Usuario.hpp
@@ -6,4 +6,24 @@ class Usuario{
 
 	public:
 	Usuario(){}
-}
+	
+	std::string getUsuario(){
+		return usuario;
+	}
+	std::string getNome(){
+		return nome;
+	}
+	std::string getSenha(){
+		return senha
+	}
+
+	void setUsuario(std::string u){
+		usuario = u;
+	}
+	void setNome(std::string n){
+		nome = n;
+	}
+	void setSenha(std::string s){
+		senha = s;
+	}
+};
