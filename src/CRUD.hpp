8822fa5d79d3f2130d8b00edb9c2633fb05a42e6
liro-1 <murilolpj@gmail.com>
2025-08-26 13:18:47 -0300

    esqueci de salvar o arquivo, agr vai

--- src/CRUD.hpp
+++ src/CRUD.hpp
@@ -1,16 +1,32 @@
 #include "Usuario.hpp"
 #include <vector>
 #include <iostream>
+#include <fstream>   //  leitura/escrita de arquivos
+#include <sstream>   //  parsear linhas
+
 
 class CRUD{
     private:
     std::vector<Usuario> usuarios;
+    std::string arquivoUsuarios = "usuarios.txt"; // arquivo
 
     public:
+    int pesquisarUsuario(std::string usuario){
+        for(int i = 0; i < usuarios.size(); i++){
+            if (usuarios[i].getUsuario() == usuario){
+                return i;
+            }
+        }
+        return -1;
+    }
     void criarUsuario(std::string user, std::string name, std::string password){
+        if (pesquisarUsuario(user) != -1) {
+        std::cout << "Erro: Nome de usuário '" << user << "' já existe." << std::endl;
+        return;
         Usuario novoUsuario(user, name, password);
         usuarios.push_back(novoUsuario);
         std::cout << "Usuário criado com sucesso" << std::endl;
+        }
     }
     void atualizarUsuario(){}
     void deletarUsuario(){}
@@ -21,16 +37,37 @@ class CRUD{
         }
         std::cout << "--- Lista de Usuários ---" << std::endl;
         for (auto user : usuarios){
-            std::cout << "Nome: " << user.getNome() << std::endl;
+            std::cout << "Nome: " << user.getNome() << " (@" << user.getUsuario() << ")" << std::endl;
         }
         std::cout << "-------------------------" << std::endl;
     }
-    int pesquisarUsuario(std::string usuario){
-        for(int i = 0; i < usuarios.size(); i++){
-            if (usuarios[i].getUsuario() == usuario){
-                return i;
-            }
+    void carregarUsuario() {
+        std::ifstream arquivo(arquivoUsuarios);
+        if (!arquivo.is_open()) {
+            return;
         }
-        return -1;
+
+        std::string linha, user, nome, senha;
+        while (std::getline(arquivo, linha)) {
+            std::stringstream ss(linha);
+            std::getline(ss, user, ';');
+            std::getline(ss, nome, ';');
+            std::getline(ss, senha, ';');
+            
+            usuarios.emplace_back(user, nome, senha); 
+        }
+        arquivo.close();
+    }
+    void salvarUsuario() {
+        std::ofstream arquivo(arquivoUsuarios);
+        if (!arquivo.is_open()) {
+            std::cerr << "Erro ao abrir o arquivo." << std::endl;
+            return;
+        }
+
+        for (const auto& user : usuarios) {
+            arquivo << user.getUsuario() << ";" << user.getNome() << ";" << user.getSenha() << "\n";
+        }
+        arquivo.close();
     }
 };
